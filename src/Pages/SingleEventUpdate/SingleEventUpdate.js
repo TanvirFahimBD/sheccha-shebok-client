0e2d169b5f01a8043003293bd8aad3da50f2af46
TanvirFahimBD <tanvirfahim6939@gmail.com>
2022-06-01 13:17:13 +0600

    up

--- src/Pages/SingleEventUpdate/SingleEventUpdate.js
+++ src/Pages/SingleEventUpdate/SingleEventUpdate.js
@@ -6,6 +6,7 @@ const SingleEventUpdate = () => {
   const {user} = useAuth()
   const { singleEventId } = useParams();
   const [event, setEvent] = useState({});
+//   const [title, setTitle] = useState(event?.title);
   const navigate = useNavigate();
 
   //Get Current Event Info
@@ -18,19 +19,53 @@ const SingleEventUpdate = () => {
       });
   }, []);
 
-  const handleVolunteerRegister = (e) => {
-    const eventRegister = {name: user.displayName, email: user.email, date: event.date, desc: event.desc, title:event.title, banner: event.banner }
-    fetch("http://localhost:5000/register", {
-      method: "POST",
+  const handleTitleChange = (e) => {
+      const newTitle = e.target.value;
+      const newEvent = {...event}
+    newEvent.title = newTitle;
+      setEvent(newEvent)
+    e.preventDefault();
+  }
+
+  const handleDateChange = (e) => { 
+    const newDate = e.target.value;
+    const newEvent = {...event}
+    newEvent.date = newDate;
+    setEvent(newEvent)
+    e.preventDefault();
+  }
+
+  const handleDescChange = (e) => { 
+    const newDesc = e.target.value;
+    const newEvent = {...event}
+    newEvent.desc = newDesc;
+    setEvent(newEvent)
+    e.preventDefault();
+  }
+
+  const handleBannerChange = (e) => { 
+    const newBanner = e.target.value;
+    // console.log("1",event.banner);
+    const newEvent = {...event}
+    newEvent.banner = newBanner;
+    setEvent(newEvent)
+    // console.log("2",event.banner);
+    e.preventDefault();
+  }
+
+  const handleEventUpdate = (e) => {
+    fetch(`http://localhost:5000/events/${singleEventId}`, {
+      method: "PUT",
       headers: {
         "content-type": "application/json",
       },
-      body: JSON.stringify(eventRegister),
+      body: JSON.stringify(event),
     })
       .then((res) => res.json())
       .then((data) => {
-       if(data?.insertedId){
-         alert("Registration Successful")
+          console.log(data);
+       if(data?.modifiedCount){
+         alert("Event Updated Successful")
          navigate("/myEvents")
        }
       });
@@ -40,33 +75,34 @@ const SingleEventUpdate = () => {
   return (
     <div>
       <h3>Update Event</h3>
-      <form onSubmit={handleVolunteerRegister}>
+      <form onSubmit={handleEventUpdate}>
       <input
           type="text"
           name=""
           id=""
           value={event.title || ""}
+          onChange={handleTitleChange}
         />
         <br />
         <input
           type="text"
           name=""
           id=""
-          value={event.date || ""}
+          value={event.date || ""} onChange={handleDateChange}
           />
         <br />
         <textarea
           type="text"
           name=""
           id=""
-          value={event.desc || ""}
+          value={event.desc || ""} onChange={handleDescChange}
           ></textarea>
           <br />
         <input
           type="text"
           name=""
           id=""
-          value={event.banner || ""}
+          value={event.banner || ""} onChange={handleBannerChange}
           />
         
         <br />
