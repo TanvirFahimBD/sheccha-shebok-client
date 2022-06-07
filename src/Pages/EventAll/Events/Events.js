c0f122b051330366449bbd2d562ed5a8ca3536de
TanvirFahimBD <tanvirfahim6939@gmail.com>
2022-06-07 21:35:13 +0600

    up

--- src/Pages/EventAll/Events/Events.js
+++ src/Pages/EventAll/Events/Events.js
@@ -3,18 +3,24 @@ import SingleEvent from "../SingleEvent/SingleEvent";
 
 const Events = () => {
   const [events, setEvents] = useState([]);
+  const [search, setSearch] = useState("");
+  // console.log(search);
   useEffect(() => {
     fetch("http://localhost:5000/events")
       .then((res) => res.json())
       .then((data) => {
         setEvents(data);
-        // console.log(data);
+        console.log("data",data);
+        const newData = data.find(dt => dt.title === search)
+        console.log("newData",newData)
       });
   }, []);
 
   return (
     <div className="text-center">
       <h1 className='my-5 text-center text-primary'  > {events.length} Events Available</h1>
+      <input type="text" name="" id="" onBlur={(e) => setSearch(e.target.value)} />
+      <input type="submit" value="Search" />
       <div className="container">
       <div className="row">
           {events.map((event) => (
